f01c983dbec80e47da1afea5b0e739e625c51c2e
vinod mali <vm21242124@gmail.com>
2025-07-10 15:31:11 +0530

    Update Experience.tsx

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -30,11 +30,11 @@ const Experience = () => {
             className="flex-1 text-black dark:text-white border-neutral-200 dark:border-slate-800"
           >
           <div className="flex flex-col items-center text-center p-3 py-6 md:p-5 lg:p-10 gap-4">
-              <img
+{/*               <img
                 src={card.thumbnail}
                 alt={card.thumbnail}
                 className="lg:w-32 md:w-20 w-16"
-              />
+              /> */}
               <div className="lg:ms-5">
                 <h1 className="text-start text-xl md:text-2xl font-bold">
                   {card.title}
